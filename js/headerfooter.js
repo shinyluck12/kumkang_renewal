3f7831b66bed44bf22e30abd16ee522249e29e41
shinyluck12 <shinyluck12@gmail.com>
2021-12-28 17:10:36 +0900

    메인,링크,제각각인패딩값수정

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -1,15 +1,18 @@
-$("html").hide();
-$("h2").hide();
-$(document).ready(function(){
+$(document).ready(function () {
+  $("html").hide();
+  $("h2").hide();
   $("html").fadeIn(300);
   $("h2").delay(400).fadeIn(400);
-    $(".h_slide").css({height:"0px"});
-    $(".h_left_btn").mouseenter(function(){
-        $("header").css({height:"180px"});
-        $(".h_slide").css({height:"120px"});
-    })
-    $(".h_left_btn").mouseleave(function(){
-        $(".h_slide").css({height:"0px"});
-        $("header").css({height:"90px"});
-    })
+  $(".h_slide").css({ height: "0px" });
+  $(".h_left_btn").mouseenter(function () {
+    $("header").css({ height: "180px" });
+    $(".h_slide").css({ height: "120px" });
+  });
+  $(".h_left_btn").mouseleave(function () {
+    $(".h_slide").css({ height: "0px" });
+    $("header").css({ height: "90px" });
+  });
+  $(".r_left").click(function () {
+    window.open("login.html");
+  });
 });
--- js/main.js
+++ js/main.js
@@ -1,36 +1,42 @@
-var swiper = new Swiper(".mySwiper", {
-    direction: "vertical",
-    slidesPerView: 1,
-    spaceBetween: 30,
-    mousewheel: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+// var swiper = new Swiper(".mySwiper", {
+//     direction: "vertical",
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     mousewheel: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
 
-function zoomIn(event) {
-    event.target.style.transform = "scale(1.2)";
-    event.target.style.transition = "all 0.5s";
-}
-
-function zoomOut(event) {
-    event.target.style.transform = "scale(1)";
-    event.target.style.transition = "all 0.5s";
-}
 $("html").hide();
-$(document).ready(function(){
+$(document).ready(function () {
   $("html").fadeIn(300);
-    $('.sec1_txt > span').hide();
-    $('.sec1_txt > h3').hide();
-    $('.sec1_txt > p').hide();
-    $('.sec1_txt > span').delay(400).fadeIn(1000);
-    $('.sec1_txt > h3').delay(900).fadeIn(1000);
-    $('.sec1_txt > p').delay(1300).fadeIn(1000);
+  $(".sec1_txt > span").hide();
+  $(".sec1_txt > h3").hide();
+  $(".sec1_txt > p").hide();
+  $(".sec1_txt > span").delay(400).fadeIn(1000);
+  $(".sec1_txt > h3").delay(900).fadeIn(1000);
+  $(".sec1_txt > p").delay(1300).fadeIn(1000);
 
-
-    // 검색버튼 나오는거
-  $(".r_search").click(function(){
-    $('.search_box').animate({width:"toggle"},400);
-  })
-})
+  $(".s2_bar_left").click(function () {
+    window.open("employ.html");
+  });
+  $(".s2_bar_right").click(function () {
+    window.open("store_search.html");
+  });
+  $(".p_box pbox1").click(function () {
+    window.open("store_search.html");
+  });
+  $(".s4_r_top").click(function () {
+    window.open("big_small.html");
+  });
+  $(".s4_r_bot").click(function () {
+    window.open("service_center.html");
+  });
+});
+// 검색버튼 나오는거
+//   $(".r_search").click(function(){
+//     $('.search_box').animate({width:"toggle"},400);
+//   })
+// })
